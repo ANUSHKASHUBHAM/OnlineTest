67c8c8ec0b49f58417dec3854c6d22f841043f31
MUSKAN1998-24 <muskan24ahuja@gmail.com>
2020-09-30 16:43:16 -0700

    change

---
+++ src/main/java/com/capgemini/onlinetestmanagementsystem/service/ITestService.java
@@ -0,0 +1,20 @@
+package com.capgemini.onlinetestmanagementsystem.service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+
+
+import com.capgemini.onlinetestmanagementsystem.entity.TestEntity;
+
+public interface ITestService {
+	
+
+	public TestEntity addTest(TestEntity test);
+	public TestEntity updateTest(BigInteger testId, TestEntity test);
+	public TestEntity deleteTest(BigInteger testId);
+	public List<TestEntity> fetchAll();
+	public TestEntity findById(BigInteger testId);
+	
+	
+}
